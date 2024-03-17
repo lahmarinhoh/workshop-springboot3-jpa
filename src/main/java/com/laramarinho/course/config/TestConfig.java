440dce2a9268852efe298fd861fa024ade4495f2
Lara Marinho <laramaricordeiro@gmail.com>
2024-03-17 19:03:10 -0300

    Payment, one-to-one association

--- src/main/java/com/laramarinho/course/config/TestConfig.java
+++ src/main/java/com/laramarinho/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.laramarinho.course.entities.Category;
 import com.laramarinho.course.entities.Order;
 import com.laramarinho.course.entities.OrderItem;
+import com.laramarinho.course.entities.Payment;
 import com.laramarinho.course.entities.Product;
 import com.laramarinho.course.entities.User;
 import com.laramarinho.course.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
+		
 
 	}
 

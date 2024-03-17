4b7033d0189318e94dee815a6249d125172ecc3f
Lara Marinho <laramaricordeiro@gmail.com>
2024-03-17 19:50:53 -0300

    Exception handling - delete

---
+++ src/main/java/com/laramarinho/course/services/exceptions/DataBaseException.java
@@ -0,0 +1,11 @@
+package com.laramarinho.course.services.exceptions;
+
+public class DataBaseException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public DataBaseException(String msg) {
+		super(msg);
+	}
+
+}
